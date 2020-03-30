263841da441dfca1eec3487c821336bb31b0b4ed
Benjamin Walt <walt@illinos.edu>
2020-03-30 13:31:44 -0500

    Added DT code

---
+++ data/data_tools/DT_ver.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+
+import numpy as np
+import os
+import json
+
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn import tree
+from sklearn.metrics import classification_report, confusion_matrix
+
+
+properties = []
+labels = []
+
+"""
+Import datasets
+"""
+
+os.chdir('/home/ben/gripper_proj_py/data/comb_data/')
+
+
+filename = 'IR1_combined_data.txt'
+with open(filename, 'r') as filehandle:
+    data_set = json.load(filehandle)
+
+
+#temp = []
+
+for data in data_set:
+    properties.append(data[0:3])
+    labels.append(data[-1])
+
+
+properties_train, properties_test, labels_train, labels_test = train_test_split(properties, labels, test_size=0.20)
+
+scaler = StandardScaler()
+scaler.fit(properties_train)
+
+properties_train = scaler.transform(properties_train)
+properties_test = scaler.transform(properties_test)
+
+clf = tree.DecisionTreeClassifier()
+
+clf.fit(properties_train, labels_train)
+
+#tree.plot_tree(clf.fit(properties_train, labels_train))
+
+
+
+labels_pred = clf.predict(properties_test)
+
+
+print(confusion_matrix(labels_test, labels_pred))
+print(classification_report(labels_test, labels_pred))
+
+
+# scaler = StandardScaler()
+# scaler.fit(properties_train)
+#
+# properties_train = scaler.transform(properties_train)
+# properties_test = scaler.transform(properties_test)
+#
+#
+# classifier = KNeighborsClassifier(n_neighbors=5)
+# classifier.fit(properties_train, labels_train)
+#
+# labels_pred = classifier.predict(properties_test)
+#
+#
+# print(confusion_matrix(labels_test, labels_pred))
+# print(classification_report(labels_test, labels_pred))
